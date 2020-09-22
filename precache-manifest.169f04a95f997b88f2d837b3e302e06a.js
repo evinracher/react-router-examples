59c40ca16dc06771808f0a94551f2fe4352cda2e
evinracher <eevinley@gmail.com>
2020-09-21 19:44:15 -0500

    Updates

---
+++ precache-manifest.169f04a95f997b88f2d837b3e302e06a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502c8a9fba87794d50e7504414948e50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a947d8cf7e22a92b7ae4",
+    "url": "/static/js/2.87df6b97.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.87df6b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62100ab711953c8bfd1",
+    "url": "/static/js/main.518da7cc.chunk.js"
+  },
+  {
+    "revision": "bea27085c435f15c17a2",
+    "url": "/static/js/runtime-main.68e66480.js"
+  }
+]);
